19c5e464132744c5815ad0dfd0644a86ed980788
Chiemela8 <chiemela8@gmail.com>
2024-11-20 07:24:03 -0800

    js odin foundations

---
+++ fjs/javascript.js
@@ -0,0 +1,42 @@
+console.log("Hi World!")
+
+let name = "Chiemela";
+let surname = "G";
+
+console.log(name);
+console.log(surname);
+
+let age = 66;
+console.log(age);
+
+age = 29;
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi); // What will be output?
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+
+// Excercises
+
+console.log(17 + 22);
+console.log(7 + 9 + 5 + 55+ 8 + 90);
+console.log((4+6+9)/77);
+
+let a = 10
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b)
+
+const max = 57;
+
+let actual = (max - 13);
+let percentage = actual / max;
+console.log(percentage);
+
+alert('Hi World!');
+console.log('Hello World!');
